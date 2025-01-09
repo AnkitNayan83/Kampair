4dfb993f138af41d44cf22c656b188195b4fb9f0
AnkitNayan13 <ankit.nayan@mathongo.com>
2025-01-09 18:52:38 +0530

    fixed ecosystem

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,17 +1,28 @@
 module.exports = {
-    apps: [
-      {
-        name: 'kampair',
-        script: 'npm',
-        args: 'start',
-        env: {
-          NODE_ENV: 'production',
-        },
-        env_production: {
-          NODE_ENV: 'production',
-        },
-        env_file: '.env', 
+  apps: [
+    {
+      name: "kampair",
+      script: "npm run start",
+      log_date_format: "MMMM D YYYY, h:mm:ss a, Z",
+      kill_timeout: 1500,
+      env: {
+        NODE_ENV: "development",
       },
-    ],
-  };
-  
+      env_production: {
+        NODE_ENV: "production",
+        PORT: 3002,
+      },
+    },
+  ],
+  deploy: {
+    production: {
+      user: "ubuntu",
+      host: "${{ secrets.EC2_PUBLIC_IP }}",
+      path: "/home/ubuntu/kampair-deploy",
+      repo: "git@github.com:AnkitNayan83/Kampair.git",  // Replace with your repo URL
+      ref: "origin/main",
+      "post-deploy":
+        "npm ci && NODE_ENV=production npm run build && pm2 reload ecosystem.config.cjs --env production",
+    },
+  },
+};
